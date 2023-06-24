e5da9fdfffbf7e013e2162714cdb5a20662a7456
ndandax <sebandandala99@gmail.com>
2023-06-24 10:24:37 +0300

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,5 @@
 file { '/etc/ssh/ssh_config':
   ensure => file,
-  mode   => '0600',
   content => "Host server\n
               IdentityFile ~/.ssh/school\n
               PasswordAuthentication no\n",
